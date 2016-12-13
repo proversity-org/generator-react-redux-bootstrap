e87440872faa852d91169869ee51a1fab5561037
jagonzalr <antonio@proversity.org>
2016-12-13 21:02:10 +0200

    basic setup

---
+++ generators/app/index.js
@@ -0,0 +1,10 @@
+
+'use strict';
+
+const generators = require('yeoman-generator');
+
+module.exports = generators.Base.extend({
+	constructor: function() {
+		generators.Base.apply(this, arguments);
+	}
+});
